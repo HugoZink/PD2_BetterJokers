968c1718809d57b3ffc88753bfadada36262e797
Hugo Zink <hugo.zink@hotmail.com>
2020-09-22 23:49:53 +0200

    Made jokers slightly more responsive

--- BetterJokers/lua/coplogicattack.lua
+++ BetterJokers/lua/coplogicattack.lua
@@ -7,3 +7,13 @@ function CopLogicAttack.chk_should_turn(data, my_data)
 
 	return not my_data.turning and not my_data.has_old_action and not data.unit:movement():chk_action_forbidden("turn") and not my_data.moving_to_cover and not my_data.walking_to_cover_shoot_pos and not my_data.surprised
 end
+
+-- Make cops less distracted when following you
+local coplogicattack_actiontaken_orig = CopLogicAttack.action_taken
+function CopLogicAttack.action_taken(data, my_data)
+	if data.unit and data.unit:base().is_convert and not data.unit:base().is_holding then
+		return true
+	end
+
+	return coplogicattack_actiontaken_orig(data, my_data)
+end
